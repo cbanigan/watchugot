1fb5e4b717e7342f3fa283d9025f5afcd98bdea5
Nick <nicholascgilpin@yahoo.com>
2017-06-08 10:03:13 -0400

    Fixed merge conflicts  Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Carbon 2017 Hackathon
 ### Set  Up
 Python - already installed on our Macs
 Pip - Type the following into the terminal:
+
 ```bash
 sudo easy_install pip
 ```
 Flask - Already installed in our repository, activate by typing:
+
 ```bash
 cd server
-source bin/activate```
+source bin/activate
+```
+Webserver
+
+Tutorial
+http://learnaholic.me/2012/10/10/installing-nginx-in-mac-os-x-mountain-lion/
--- server/server/source/server.py
+++ server/server/source/server.py
@@ -1,32 +1,42 @@
-import socket
-import sys
- 
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 1234 # Arbitrary non-privileged port
- 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
- 
-#Bind socket to local host and port
-try:
-    socket.bind((HOST, PORT))
-except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
-print 'Socket bind complete'
- 
-#Start listening on socket
-socket.listen(10)
-print 'Socket now listening'
- 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = socket.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
-socket.close()
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
 
+@app.route("/login/<username>/<eventid>", methods=["GET", "POST"])
+def login(username, eventid):
+	data = {"user": username,
+			"isLoggedIn": 1,
+			"event":eventid}
+	return jsonify(data)
 
+app.run()
 
+# import socket
+# import sys
+#  
+# HOST = ''   # Symbolic name, meaning all available interfaces
+# PORT = 1234 # Arbitrary non-privileged port
+#  
+# socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print 'Socket created'
+#  
+# #Bind socket to local host and port
+# try:
+#     socket.bind((HOST, PORT))
+# except socket.error as msg:
+#     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+#     sys.exit()
+#      
+# print 'Socket bind complete'
+#  
+# #Start listening on socket
+# socket.listen(10)
+# print 'Socket now listening'
+#  
+# #now keep talking with the client
+# while 1:
+#     #wait to accept a connection - blocking call
+#     conn, addr = socket.accept()
+#     print 'Connected with ' + addr[0] + ':' + str(addr[1])
+#      
+# socket.close()
